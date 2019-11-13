ea59407e6069eeee3f7138f78a7cf0792dbfeea2
hillyerc <57057188+hillyerc@users.noreply.github.com>
2019-11-12 19:14:44 -0600

    Update InsuranceScore.java

--- HillyerNilesTempestini/InsuranceScore.java
+++ HillyerNilesTempestini/InsuranceScore.java
@@ -1,5 +1,49 @@
-package HillyerNilesTempestini;
+import java.util.ArrayList;
 
 public class InsuranceScore {
+	private String name;
+	private int score;
+	private String riskLevel;
+	ArrayList <InsuranceScore> scores = new ArrayList<InsuranceScore>();
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getRiskLevel() {
+		return riskLevel;
+	}
+	public void setRiskLevel(String riskLevel) {
+		
+		this.riskLevel = riskLevel;
+	}
+	
+	public InsuranceScore(String name, int score, String riskLevel) {
+		setName(name);
+		setScore(score);
+		setRiskLevel(riskLevel);
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s %s %s", getName(), getScore(), getRiskLevel());
+	}
+	
+	public static void PrintScoreCard(ArrayList<InsuranceScore> IS) {
+        for (InsuranceScore temp : IS) {
+            System.out.println(temp);
+        }
+	
+	}
+	
 
 }
